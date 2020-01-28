bdf1a8522f2f0b2db1d052367ee58fc05807a2c5
Combatd <combatdstar@gmail.com>
2020-01-27 17:34:55 -0800

    Set attributes in people table for database migration

--- db/migrate/20200128004336_create_person.rb
+++ db/migrate/20200128004336_create_person.rb
@@ -1,6 +1,10 @@
 class CreatePerson < ActiveRecord::Migration[5.2]
   def change
+    add_column :applications :name :house_id
+
     create_table :people do |t|
+      t.string :name
+      t.integer :house_id
     end
   end
 end
