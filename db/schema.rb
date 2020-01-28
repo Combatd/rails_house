70868b067159998b64bf3cd9497f264738a65355
Combatd <combatdstar@gmail.com>
2020-01-28 11:37:29 -0800

    Migrate CreateHouse to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_004336) do
+ActiveRecord::Schema.define(version: 2020_01_28_182848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"
